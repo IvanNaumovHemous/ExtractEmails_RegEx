0d477f21ee7ac5b5da3bdd31372747ccb91b1579
Ivan <ivan.naumov92@yahoo.com>
2018-06-24 17:36:07 +0300

    Import

---
+++ ExtractEmails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var RegexMatches = GetRegexMatches();
+            PrintRegexMatches(RegexMatches);
+        }
+
+        private static void PrintRegexMatches(MatchCollection regexMatches)
+        {
+            foreach (Match item in regexMatches)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static MatchCollection GetRegexMatches()
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\w+.\w+@\w+.\w+\.?\w+";
+
+            var matches = Regex.Matches(text, pattern);
+            return matches;
+        }
+    }
+}
